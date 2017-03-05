b7f28e3373077b0bf855a1328a331fb4420f2be4
김병곤 <fharenheit@gmail.com>
2017-03-06 01:01:22 +0900

    Product 처리 코드 추가

--- src/main/java/com/exem/bigdata/template/spark/JavaSparkProduct.java
+++ src/main/java/com/exem/bigdata/template/spark/JavaSparkProduct.java
@@ -9,10 +9,6 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.Metadata;
-import org.apache.spark.sql.types.StructField;
-import org.apache.spark.sql.types.StructType;
 import org.apache.spark.storage.StorageLevel;
 
 import java.util.ArrayList;
@@ -39,17 +35,10 @@ public final class JavaSparkProduct extends AbstractJob {
 
     @Override
     protected void processing(SparkSession sparkSession) throws Exception {
-        StructType schema = new StructType(
-                new StructField[]{
-                        new StructField("GROUPED_KEY", DataTypes.StringType, true, Metadata.empty()),
-                        new StructField("PRODUCT_CLASSIFICATION", DataTypes.StringType, true, Metadata.empty()),
-                        new StructField("PRODUCT_NM", DataTypes.StringType, true, Metadata.empty()),
-                        new StructField("BRAND_LINE", DataTypes.StringType, true, Metadata.empty())
-                }
-        );
-
         JavaSparkContext jsc = new JavaSparkContext(sparkSession.sparkContext());
         JavaRDD<String> stringRDD = jsc.textFile("product.txt").persist(StorageLevel.MEMORY_AND_DISK());
+
+        // CSV 파일을 로딩하여 Product RDD를 생성한다.
         JavaRDD<Product> products = stringRDD.map(new Function<String, Product>() {
             @Override
             public Product call(String row) throws Exception {
@@ -63,20 +52,22 @@ public final class JavaSparkProduct extends AbstractJob {
 
                 return product;
             }
-        }).cache();
+        });
 
         System.out.println(products.count());
 
+        // Product RDD를 Group By한다.
         JavaPairRDD<String, Iterable<Product>> pairRDD = products.groupBy(new Function<Product, String>() {
             @Override
-            public String call(Product p) throws Exception {
-                return p.GROUPED_KEY;
+            public String call(Product product) throws Exception {
+                return product.GROUPED_KEY;
             }
         });
 
         System.out.println(pairRDD.count());
         System.out.println(pairRDD.collectAsMap());
 
+        // Group By한 Product RDD 집합을 조건에 맞는 것들을 필터링한다.
         JavaPairRDD<String, List<Product>> stringListJavaPairRDD = pairRDD.mapValues(new Function<Iterable<Product>, List<Product>>() {
             @Override
             public List<Product> call(Iterable<Product> products) throws Exception {
@@ -84,14 +75,22 @@ public final class JavaSparkProduct extends AbstractJob {
                 Iterator<Product> iterator = products.iterator();
                 while (iterator.hasNext()) {
                     Product product = iterator.next();
+                    // 여기는 확장되어야 한다.
                     if (product.GROUPED_KEY.equals("101112")) list.add(product);
                 }
                 return list;
             }
         });
 
-        List<List<Product>> collect = stringListJavaPairRDD.values().collect();
-        System.out.println(collect);
+        // Group By한 데이터에서 건수가 0인것을 제외하고 모두 합친다.
+        List<List<Product>> collected = stringListJavaPairRDD.values().collect();
+        List<Product> finalProducts = new ArrayList();
+        for (List<Product> p : collected) {
+            if (p.size() > 0) {
+                finalProducts.addAll(p);
+            }
+        }
+        System.out.println(finalProducts);
     }
 
     public static void main(String[] args) throws Exception {
