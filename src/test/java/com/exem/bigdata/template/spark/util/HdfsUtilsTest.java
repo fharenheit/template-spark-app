1acacef3d67f1efbbad23a42bd9b6f2982c5a69e
김병곤 <fharenheit@gmail.com>
2017-03-05 15:44:40 +0900

    유틸리티 추가

--- src/test/java/com/exem/bigdata/template/spark/util/HdfsUtilsTest.java
+++ src/test/java/com/exem/bigdata/template/spark/util/HdfsUtilsTest.java
@@ -1,8 +1,10 @@
 package com.exem.bigdata.template.spark.util;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.data.hadoop.fs.HdfsResourceLoader;
 
 import java.io.IOException;
 
@@ -20,4 +22,11 @@ public class HdfsUtilsTest {
         Assert.assertEquals("0.23.0", conf.get("hadoop.common.configuration.version"));
     }
 
+    @Test
+    public void getHdfsResourceLoader() throws IOException {
+        Configuration conf = HdfsUtils.getConfiguration("classpath:com/exem/bigdata/template/spark/util/core-default.xml");
+        HdfsResourceLoader loader = new HdfsResourceLoader(conf);
+        FileSystem fs = loader.getFileSystem();
+        Assert.assertEquals("org.apache.hadoop.fs.LocalFileSystem", fs.getClass().getName());
+    }
 }
