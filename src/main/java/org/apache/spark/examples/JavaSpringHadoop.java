da54a8f36c9af16fee8760e6751fbff5b2660261
김병곤 <fharenheit@gmail.com>
2017-04-13 01:40:09 +0900

    Spring + MyBATIS 추가

---
+++ src/main/java/org/apache/spark/examples/JavaSpringHadoop.java
@@ -0,0 +1,16 @@
+package org.apache.spark.examples;
+
+import org.apache.spark.examples.repository.DummyRepository;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+public class JavaSpringHadoop {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:/hadoop/applicationContext*.xml");
+    }
+
+}
